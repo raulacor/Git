0d45bf5d61e6a3dc37c2c249c30ba1d9aa31ade0
Raul <raul.azzi.corsi@gmail.com>
2025-08-27 12:50:14 -0300

    Fist EX

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
