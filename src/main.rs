380c928e16c15c57cef52c769db084a6387bde00
Raul <raul.azzi.corsi@gmail.com>
2025-08-27 12:58:06 -0300

    Initialize .git

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-#[allow(unused_imports)]
+use clap::{Subcommand, Parser}
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+
+
+
+
+//Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(Subcomand)]
+    command: Command
+}
+
+/// Doc comment
+#[derive(Debug, Subcomand)]
+enum command {
+    Init,
+}
 
 fn main() {
     eprintln!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+    match on Args.command {
+        Command::Init => {
+         fs::create_dir(".git").unwrap();
+         fs::create_dir(".git/objects").unwrap();
+         fs::create_dir(".git/refs").unwrap();
+         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+         println!("Initialized git directory")
+        }
+    }
 }
